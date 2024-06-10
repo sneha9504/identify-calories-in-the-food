65f0d5237283857e770d12476181aad7932f9119
sneha9504 <snehawani4321@gmail.com>
2024-06-10 21:31:36 +0530

    Update health.py

--- health.py
+++ health.py
@@ -12,7 +12,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## Function to load Google Gemini Pro Vision API And get response
 
-def get_gemini_repsonse(input,image,prompt):
+def get_gemini_response(input,image,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
     response=model.generate_content([input,image[0],prompt])
     return response.text
